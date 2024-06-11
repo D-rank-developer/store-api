dcf4a59b673785200a84f2c31ca7145174e02304
Dumanyie Chamberlain <93911676+Dumanyie-18CJ024115@users.noreply.github.com>
2024-06-11 15:21:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+require("dotenv").config()
+//async errors
+require("express-async-errors")
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+//middewares
+
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products routes</a>');
+})
+
+app.use("/api/v1/products",productRouter);
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port =process.env.PORT || 3000
+
+const start= async () => { // this is the fuction to connect the database
+    try {
+
+        await connectDB(process.env.MONGO_URI);
+
+        app.listen(port, console.log(`server is running at ${port}...`))
+        
+    } catch (error) {
+
+        console.log(error)
+
+        
+    }
+}
+
+start();
